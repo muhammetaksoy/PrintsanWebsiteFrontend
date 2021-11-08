83fd7b67d85cc32ebfd2da8a1aff89017a5a1cfa
Muratcan <muratcan.tufekci@hotmail.com>
2021-11-08 22:31:54 +0300

    carousel deneme

--- src/Pages/Anasayfa/Components/Clients.js
+++ src/Pages/Anasayfa/Components/Clients.js
@@ -1,5 +1,6 @@
 import classes from './Clients.module.css';
 
+
 const Clients = () => {
     let className = classes.img;
     return(
@@ -7,31 +8,26 @@ const Clients = () => {
             <div className="container">
 
                 <div className="row">
-
-                <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img className={className} src={require("../../../images/client-1.png").default}  alt="aaa"/>
-                </div>
-
-                <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img className={className} src={require("../../../images/client-2.png").default}  alt="aaa"/>
-                </div>
-
-                <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img className={className} src={require("../../../images/client-3.png").default}  alt="aaa"/>
-                </div>
-
-                <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img className={className} src={require("../../../images/client-4.png").default}  alt="aaa"/>
-                </div>
-
-                <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img className={className} src={require("../../../images/client-5.png").default}  alt="aaa"/>
-                </div>
-
-                <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img className={className} src={require("../../../images/client-6.png").default}  alt="aaa"/>
-                </div>
-
+                    
+                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        <img className={className} src={require("../../../images/client-1.png").default}  alt="aaa"/>
+                    </div>               
+                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        <img className={className} src={require("../../../images/client-2.png").default}  alt="aaa"/>
+                    </div>              
+                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        <img className={className} src={require("../../../images/client-3.png").default}  alt="aaa"/>
+                    </div>                
+                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        <img className={className} src={require("../../../images/client-4.png").default}  alt="aaa"/>
+                    </div>              
+                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        <img className={className} src={require("../../../images/client-5.png").default}  alt="aaa"/>
+                    </div>       
+                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        <img className={className} src={require("../../../images/client-6.png").default}  alt="aaa"/>
+                    </div>
+                    
                 </div>
 
             </div>
