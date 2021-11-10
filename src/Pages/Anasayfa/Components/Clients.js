b203a151c1e01382a0cf65d503fb6bf431d431a4
muhammmetaksoy <muhammetaksoy176@gmail.com>
2021-11-10 22:51:53 +0300

    Company slider edited

--- src/Pages/Anasayfa/Components/Clients.js
+++ src/Pages/Anasayfa/Components/Clients.js
@@ -1,33 +1,66 @@
 import classes from './Clients.module.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
 const Clients = () => {
     let className = classes.img;
-    return(
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+            slidesToSlide: 1 // optional, default to 1.
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        }
+    };
+
+    return (
         <section className={classes.section}>
+
+
+
             <div className="container">
                 <h2 className={classes.h2}>REFERANSLAR</h2>
                 <div className="row">
-                    
-                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                        <img className={className} src={require("../../../images/client-1.png").default}  alt="aaa"/>
-                    </div>               
-                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                        <img className={className} src={require("../../../images/client-2.png").default}  alt="aaa"/>
-                    </div>              
-                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                        <img className={className} src={require("../../../images/client-3.png").default}  alt="aaa"/>
-                    </div>                
-                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                        <img className={className} src={require("../../../images/client-4.png").default}  alt="aaa"/>
-                    </div>              
-                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                        <img className={className} src={require("../../../images/client-5.png").default}  alt="aaa"/>
-                    </div>       
-                    <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                        <img className={className} src={require("../../../images/client-6.png").default}  alt="aaa"/>
-                    </div>
-                    
+                    <Carousel
+                        className
+                        infinite={true}
+                        responsive={responsive}
+                        autoPlay={true}
+                        removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
+                    >
+
+                        <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                            <img className={className} src={require("../../../images/client-1.png").default} alt="aaa" />
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                            <img className={className} src={require("../../../images/client-2.png").default} alt="aaa" />
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                            <img className={className} src={require("../../../images/client-3.png").default} alt="aaa" />
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                            <img className={className} src={require("../../../images/client-4.png").default} alt="aaa" />
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                            <img className={className} src={require("../../../images/client-5.png").default} alt="aaa" />
+                        </div>
+                        <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                            <img className={className} src={require("../../../images/client-6.png").default} alt="aaa" />
+                        </div>
+
+                    </Carousel>
+
+
                 </div>
 
             </div>
--- src/Pages/Anasayfa/Components/slider.js
+++ src/Pages/Anasayfa/Components/slider.js
@@ -4,13 +4,25 @@ export default class Slider extends Component {
         return (
             <div id="carouselExampleCaptions" className="carousel  slide " data-bs-ride="carousel">
                 <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active bg-danger" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" className="active bg-danger" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" className="active bg-danger" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active bg-danger" aria-current="true" aria-label="Slide 0"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" className="active bg-danger" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" className="active bg-danger" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" className="active bg-danger" aria-label="Slide 3"></button>
                 </div>
                 <div className="carousel-inner">
+
                     <div className="carousel-item active">
-                        <img alt="deneme" src="https://fr8wff00-a.akamaihd.net/philippines/wp-content/uploads/2021/05/Work-From-Home-Jobs-5.png" className="d-block w-100" />
+                        <img alt="deneme" src={require("../../../images/Slider4.jpeg").default} alt="aaa" className="d-block w-100" />
+                        <div className="carousel-caption d-flex h-100 align-items-center justify-content-center">
+                            <div>
+                                <h1>Welcome To Printsan</h1>
+                                <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="carousel-item ">
+                        <img alt="deneme" src={require("../../../images/Slider1.jpeg").default} alt="aaa" className="d-block w-100" />
                         <div className="carousel-caption d-flex h-100 align-items-center justify-content-center">
                             <div>
                                 <h1>Welcome To Printsan</h1>
@@ -21,7 +33,7 @@ export default class Slider extends Component {
                         </div>
                     </div>
                     <div className="carousel-item">
-                        <img alt="deneme" src="https://fr8wff00-a.akamaihd.net/philippines/wp-content/uploads/2021/05/Work-From-Home-Jobs-5.png" className="d-block w-100" />
+                        <img alt="deneme" src={require("../../../images/Slider2.jpeg").default} alt="aaa" className="d-block w-100" />
                         <div className="carousel-caption d-flex h-100 align-items-center justify-content-center">
                             <div>
                                 <h1>Welcome To Printsan</h1>
@@ -31,7 +43,7 @@ export default class Slider extends Component {
                         </div>
                     </div>
                     <div className="carousel-item">
-                        <img alt="deneme" src="https://fr8wff00-a.akamaihd.net/philippines/wp-content/uploads/2021/05/Work-From-Home-Jobs-5.png" className="d-block w-100" />
+                        <img alt="deneme" src={require("../../../images/Slider3.jpeg").default} alt="aaa" className="d-block w-100" />
                         <div className="carousel-caption d-flex h-100 align-items-center justify-content-center">
                             <div>
                                 <h1>Welcome To Printsan</h1>
@@ -40,6 +52,7 @@ export default class Slider extends Component {
 
                         </div>
                     </div>
+
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
