8939e4f4ae6728b5d556bfcd3d5a69d968d8671b
Muratcan <muratcan.tufekci@hotmail.com>
2021-11-10 22:11:06 +0300

    Count-up revize

--- src/Pages/Anasayfa/Components/Counter.js
+++ src/Pages/Anasayfa/Components/Counter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CountUp from 'react-countup';
 import classes from './Counter.module.css';
 import { IoHappyOutline,IoAccessibilityOutline,IoAnalyticsOutline } from "react-icons/io5";
+import VisibilitySensor from 'react-visibility-sensor';
 
 const Counter = () => {
    
@@ -11,22 +12,43 @@ const Counter = () => {
                 <div className="row text-center">
                     <div className="col-md-3">
                         <IoAnalyticsOutline className={classes.icon}/>
-                        <h2><CountUp end={45000} duration={1}/></h2>
+                        <h2><CountUp end={45000} duration={1.5}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor onChange={start} delayedCall>
+                                    <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}</CountUp></h2>
                         <h3>GÜNLÜK ÜRETİM</h3>
                     </div>
                     <div className="col-md-3">
                         <IoAnalyticsOutline className={classes.icon} />
-                        <h2><CountUp end={1300000} duration={1}/></h2>
+                        <h2><CountUp end={1300000} duration={1.5} >
+                                {({ countUpRef, start }) => (
+                                <VisibilitySensor onChange={start} delayedCall>
+                                    <span ref={countUpRef} />
+                                </VisibilitySensor>
+                                )}
+                            </CountUp></h2>
                         <h3>AYLIK ÜRETİM</h3>
                     </div>
                     <div className="col-md-3">
                         <IoHappyOutline className={classes.icon}/>
-                        <h2><CountUp end={200} duration={1}/></h2>
+                        <h2><CountUp end={200} duration={1.5}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor onChange={start} delayedCall>
+                                    <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}</CountUp></h2>
                         <h3>ANLAŞMALI MARKA</h3>
                     </div>
                     <div className="col-md-3">
                         <IoAccessibilityOutline className={classes.icon}/>
-                        <h2><CountUp end={50} duration={1}/></h2>
+                        <h2><CountUp end={50} duration={1.5}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor onChange={start} delayedCall>
+                                    <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}</CountUp></h2>
                         <h3>ÇALIŞAN</h3>
                     </div>
                 </div>
