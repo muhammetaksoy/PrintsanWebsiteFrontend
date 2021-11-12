10f82253caeccae61eaa4f9adcf316575aa6680f
Muhammet Aksoy <muhammet.aksoy@lcwaikiki.com>
2021-11-12 10:00:18 +0300

    autocomplete özelliği eklendi

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Slider from "./Pages/Anasayfa/Components/Slider";
 import Company from "./Pages/Anasayfa/Components/Company";
 import Counter from "./Pages/Anasayfa/Components/Counter";
 import Clients from "./Pages/Anasayfa/Components/Clients";
-import Footer from "./Pages/Anasayfa/Components/Footer";
 import Vision from "./Pages/Anasayfa/Components/Vision";
+import React from "react";
+import Footer from "Pages/Anasayfa/Components/Footer";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Slider />
       <Clients />
       <Company />
-      <Vision/>
+      <Vision />
       <Counter />
       <Footer />
     </div>
