4f1826b7739ea60ed21f0824c5a94aee134ebb0d
muhammmetaksoy <muhammetaksoy176@gmail.com>
2021-11-24 00:19:32 +0300

    video kısmı tamamlandı

--- src/Pages/Anasayfa/Components/Video.js
+++ src/Pages/Anasayfa/Components/Video.js
@@ -1,33 +1,50 @@
 import { Alert } from '@mui/material';
+import { width } from '@mui/system';
 import * as React from 'react';
+import { useState } from 'react';
+import ModalVideo from 'react-modal-video'
+import 'react-modal-video/css/modal-video.css';
+import { FaPlayCircle } from "react-icons/fa";
 
 export default function Video() {
+    const [isOpen, setOpen] = useState(false)
     return (
 
-        <div className="container-fluid ">
-            <div className="row m-4">
-                <div className="col-lg-6 col-md-6 col-6 d-flex align-items-center justify-content-center"
-                    style={{ backgroundColor: "#F8F9FA" }}>
+        <div className="container-fluid w-75">
+            <div className="row " >
 
-                    <Alert icon={false} variant="outlined" severity="error">
 
-                        <h2 style={{ fontSize: 40, fontStyle: "italic" }}>
-                            PRİNTSAN TANITIM VİDEOUSU
-                        </h2>
-                    </Alert>
+                <div className="col-lg-4 col-md-6 col-12 ">
+
+                    <div style={{ textAlign: "center", "height": "100%", display: "flex", justifyContent: "center", "alignItems": "center" }}>
+                        <p style={{ fontSize: 40, }}>
+                            PRİNTSAN TANITIM
+                        </p>
+                    </div>
                 </div>
-                <div className="col-lg-6 col-md-6 col-6 d-flex align-items-center justify-content-center">
-                    <iframe
-                        allowFullScreen={true}
-                        width="720" height="360"
-                        src="https://www.youtube.com/embed/j-6I5VhjY2U"
-                        title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-                    </iframe>
+                <div className="col-lg-4 col-md-6 col-12 " >
+                    <div style={{ textAlign: "center", "height": "100%", display: "flex", justifyContent: "center", "alignItems": "center" }}>
+                        <React.Fragment>
+                            <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="j-6I5VhjY2U" onClose={() => setOpen(false)} />
+                            <FaPlayCircle onClick={() => setOpen(true)} style={{ fontSize: "120", color: "#d9232d" }} />
+                        </React.Fragment>
+
+                    </div>
 
                 </div>
+                <div className="col-lg-4 col-md-6 col-12 " >
+
+                    <img style={{ height: "400px", "width": "100%" }} alt="deneme" src={require("../../../images/bina.png").default} />
+                </div>
+
+
+
+
             </div>
+
+
         </div>
+
 
     );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
