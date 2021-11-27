04e10552181f372d63af6874a731a61aefd2db66
Muhammet Aksoy <muhammet.aksoy@lcwaikiki.com>
2021-11-27 18:01:04 +0300

    edit

---
+++ src/Pages/MakinaParkuru/machineParkour.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './machineParkour.module.css';
+
+const machineParkour = () => {
+    return (
+        <section className={classes.section}>
+
+        </section>
+    )
+
+}
+export default machineParkour;
