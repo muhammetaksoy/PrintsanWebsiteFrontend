b39839cbeaab2c68f1dc07da077cf8d097573067
Muhammet Aksoy <muhammet.aksoy@lcwaikiki.com>
2021-11-27 17:50:51 +0300

    edit

--- src/Pages/SirketProfili/Products.js
+++ src/Pages/SirketProfili/Products.js
@@ -11,7 +11,6 @@ export default function Products() {
             .then(response => setProducts(response))
     }, []);
 
-
     const productsAll = products.map(item => (
         <div>
             <div className="card border-0" >
@@ -29,11 +28,25 @@ export default function Products() {
 
     return (
         <section style={{ padding: "40px 0px", overflow: "hidden" }}>
-            <h2 className="text-center container-fluid  p-2 alert alert-danger border-0 rounded-0 " style={{ "width": "85%", "fontSize": "2.8rem" }}>Ürünler</h2>
-            <div className="container-fluid " style={{ 'width': "85%" }} >
-                <div className="d-flex justify-content-between flex-wrap ">
-                    {productsAll}
+            <h2 className="text-center container-fluid  p-2 alert alert-danger border-0 rounded-0 " style={{ "fontSize": "2.8rem" }}>Ürünler</h2>
+            <div className="container-fluid "  >
+                <div className="row">
+                    <div className="col-md-2">
+                        <ul className="list-group ">
+                            <li className="list-group-item active ">Emprime Baskı</li>
+                            <li className="list-group-item">Enjeksiyon</li>
+                            <li className="list-group-item">Morbi leo risus</li>
+                            <li className="list-group-item">Porta ac consectetur ac</li>
+                            <li className="list-group-item">Vestibulum at eros</li>
+                        </ul>
+                    </div>
+                    <div className="col-md-10">
+                        <div className="d-flex justify-content-between flex-wrap ">
+                            {productsAll}
+                        </div>
+                    </div>
                 </div>
+
 
             </div>
         </section>
