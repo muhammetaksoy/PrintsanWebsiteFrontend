a4683793905ffcb7d390f57366c0716c1fd7ea39
muhammmetaksoy <muhammetaksoy176@gmail.com>
2021-12-02 19:11:59 +0300

    Medya sayfası olusturuldu

--- src/Pages/Medya/Media.js
+++ src/Pages/Medya/Media.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Media.module.css';
-
 const Media = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        fetch("http://printsanaccess.online/api/Explore/GetArticles")
+            .then((response) => response.json())
+            .then(response => {
+                console.log(response)
+                setArticles(response)
+            })
+    }, []);
 
     return (
-        <section className={classes.section}>
+        <div className={classes.container}>
+            <h2 className="text-center w-100  p-2 alert alert-danger border-0 rounded-0 " style={{ "width": "85%", "fontSize": "2.8rem" }}>Medya</h2>
+            {
+                articles.map(item => (
+                    <div className={classes.card}>
+                        <div>
+                            <img src={item.PhotoUrl1} className={classes.img} width="600" height="200" />
+                        </div>
+                        <div className={classes.cardBody}>
+                            <span className={classes.tagRed}>{item.PublishedDate.slice(0, 10)}</span>
+                            <h4>{item.Header}</h4>
+                            <p>{item.Text.substring(0, 150)}...</p>
+                        </div>
+                        <div className={classes.cardFooter}>
+                            <a className={classes.button}>Detay</a>
+                        </div>
+                    </div>
+                ))
+            }
 
-        </section>
+        </div>
     )
 
 }
