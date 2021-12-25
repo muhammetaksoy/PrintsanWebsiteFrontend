e5213f3a52faead5c6fc2dde3b71b888f1a335f3
muhammmetaksoy <muhammetaksoy176@gmail.com>
2021-12-25 16:45:55 +0300

    login tasarım

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,68 +1,96 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './Login.css';
-
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./Login.css";
+import { Link } from "react-router-dom";
 
 async function loginUser(credentials) {
-
-    return fetch('http://printsanaccess.online/getToken', {
-      
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        'userName': credentials.userName,
-        'password':credentials.password,
-        'grant_type': 'password'
-    })       
+  return fetch("http://printsanaccess.online/getToken", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      userName: credentials.userName,
+      password: credentials.password,
+      grant_type: "password",
+    }),
   })
-
-  .then(response=>{
-    if(response.status===200){
-      window.location.assign("http://localhost:3000/Admin")
-    }
-    return response.json();
-  }).then(data=>data.access_token)
-
-
-
-} 
-
+    .then((response) => {
+      if (response.status === 200) {
+        window.location.assign("http://localhost:3000/Admin");
+      }
+      return response.json();
+    })
+    .then((data) => data.access_token);
+}
 
 export default function Login({ setToken }) {
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        const token = await loginUser({
-            userName,
-            password,
-            grant_type:"password"
-        });
-        
-        setToken(token);
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser({
+      userName,
+      password,
+      grant_type: "password",
+    });
 
-  return(
-    <form onSubmit={handleSubmit}>
-      <label>
-        <p>Username</p>
-        <input type="text" onChange={e => setUserName(e.target.value)} />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" onChange={e => setPassword(e.target.value)} />
-      </label>
-      <div>
-        <button type="submit" >Submit</button>
-      </div>
-    </form>
-  )
+    setToken(token);
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light px-5 ">
+        <div className="container-fluid ">
+          <img
+            style={{ height: "90px", width: "220px" }}
+            className="navbar-brand"
+            alt="printsanlogo"
+            src={require("../../images/printsanlogo.png").default}
+          />
+
+          {/* <Link to="/Anasayfa" className="nav-link " aria-current="page">
+            Anasayfaya Git
+          </Link> */}
+        </div>
+      </nav>
+
+      <form onSubmit={handleSubmit} className="login-wrapper">
+        <h1 className="text-center">Admin Giriş Sayfası</h1>
+        <div className="container">
+          <label>
+            <b>Kullanıcı Adı</b>
+          </label>
+          <input
+            className="inputStyle"
+            type="text"
+            placeholder="Kullanıcı Adını giriniz"
+            required
+            onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <label>
+            <b>Şifre</b>
+          </label>
+          <input
+            className="inputStyle"
+            type="password"
+            placeholder="Şifreyi Giriniz"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="buttonStyle" type="submit">
+            Giriş Yap
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 Login.propTypes = {
-    setToken: PropTypes.func.isRequired
-}
+  setToken: PropTypes.func.isRequired,
+};
